c5f35251bd6bde8c81f618b9ab0f09a7de23cbc7
Liam Bakar <liambakar@liams-mbp.lan>
2023-03-22 18:46:07 -0400

    Added functionality for the filtering, adding and updating the filters should be easy from here on out

--- src/Pages/ExploreComponents/FilterBar.jsx
+++ src/Pages/ExploreComponents/FilterBar.jsx
@@ -1,43 +1,103 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { useState, useEffect } from "react";
+import {
+    typeFilterKeys,
+    languageFilterKeys,
+    scriptFilterKeys,
+    genreFilterKeys,
+} from "./FilterUtils";
 
 const FilterBar = (props) => {
+    const [typeFilter, setTypeFilter] = useState(null);
+    const handleTypeFilterChange = (e) => {
+        setTypeFilter(e.target.value);
+    };
+
+    const [languageFilter, setLanguageFilter] = useState(null);
+    const handleLanguageFilterChange = (e) => {
+        setLanguageFilter(e.target.value);
+    };
+
+    const [scriptFilter, setScriptFilter] = useState(null);
+    const handleScriptFilterChange = (e) => {
+        setScriptFilter(e.target.value);
+    };
+
+    const [genreFilter, setGenreFilter] = useState(null);
+    const handleGenreFilterChange = (e) => {
+        setGenreFilter(e.target.value);
+    };
+
+    // Handles the lag in asynchronous function setState()
+    useEffect(() => {
+        props.filterByType(typeFilter);
+        props.filterByLanguage(languageFilter);
+        props.filterByScript(scriptFilter);
+        props.filterByGenre(genreFilter);
+    });
     return (
         <div className='grid'>
             <div>
                 <SearchBar onChange={props.searchChange} />
             </div>
-            <div>
-                <select id='fruit' required>
-                    <option value='' selected>
-                        Select a fruit…
+            <div className='headings'>
+                <small>Type:</small>
+                <select id='type' required onChange={handleTypeFilterChange}>
+                    <option value={null} selected>
+                        All
                     </option>
-                    <option>…</option>
+                    {typeFilterKeys.map((key) => {
+                        return <option value={key}>{key}</option>;
+                    })}
                 </select>
             </div>
-            <div>
-                <select id='beet' required>
-                    <option value='' selected>
-                        Select a fruit…
+            <div className='headings'>
+                <small>Language:</small>
+                <select
+                    id='language'
+                    required
+                    onChange={handleLanguageFilterChange}
+                >
+                    <option value={null} selected>
+                        All
                     </option>
-                    <option>…</option>
+                    {languageFilterKeys.map((key) => {
+                        return <option value={key}>{key}</option>;
+                    })}
                 </select>
             </div>
-            <div>
-                <select id='carrot' required>
-                    <option value='' selected>
-                        Select a fruit…
+            <div className='headings'>
+                <small>Script:</small>
+                <select
+                    id='script'
+                    required
+                    onChange={handleScriptFilterChange}
+                >
+                    <option value={null} selected>
+                        All
                     </option>
-                    <option>…</option>
+                    {scriptFilterKeys.map((key) => {
+                        return <option value={key}>{key}</option>;
+                    })}
                 </select>
             </div>
-            <div>
-                <select id='potato' required>
-                    <option value='' selected>
-                        Select a fruit…
+            <div className='headings'>
+                <small>Genre:</small>
+                <select id='genre' required onChange={handleGenreFilterChange}>
+                    <option value={null} selected>
+                        All
                     </option>
-                    <option>…</option>
+                    {genreFilterKeys.map((key) => {
+                        return <option value={key}>{key}</option>;
+                    })}
                 </select>
+            </div>
+            <div className='headings'>
+                <small>Date:</small>
+            </div>
+            <div className='headings'>
+                <small>Dimensions:</small>
             </div>
         </div>
     );
---
+++ src/Pages/ExploreComponents/FilterUtils.js
@@ -0,0 +1,26 @@
+export const typeFilterKeys = ["Manuscript", "Print"];
+
+export const languageFilterKeys = ["Turkish", "Arabic"];
+
+export const scriptFilterKeys = [
+    "Nesihle",
+    "Talikle",
+    "Rika",
+    "Divani",
+    "Küçük boy",
+    "Nestalikle",
+];
+
+export const genreFilterKeys = ["option"];
+
+// Table columns being searched by
+export const searchKeys = [
+    "authorNameOriginal",
+    "authorNameTranslit",
+    "titleOriginal",
+    "titleTranslit",
+    "language",
+    "genre",
+    "textType",
+    "date",
+];
